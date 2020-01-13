1e921e09543b15b80d1cce0d83933f2c10a6acd2
kshithijiyer <kshithij.ki@gmail.com>
2020-01-13 19:00:30 +0530

    [Fix] Removing additonal underscore(_) from testcase name.
    Change-Id: Idcc40442869cb3e44873625887409592d9e0710d
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>

--- tests/functional/afr/test_self_heal_when_dir_quota_exceeded.py
+++ tests/functional/afr/test_self_heal_when_dir_quota_exceeded.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
+#  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -91,7 +91,7 @@ class HealFilesWhenDirQuotaExceeded(GlusterBaseClass):
 
         cls.get_super_method(cls, 'tearDownClass')()
 
-    def test_heal_when_dir_quota_exceeded_(self):
+    def test_heal_when_dir_quota_exceeded(self):
         # Create a directory to set the quota_limit_usage
         path = "/dir"
         g.log.info("Creating a directory")
