3459fb99ff3a46dd3d1c702922758b3b6ab430ea
kshithijiyer <kshithij.ki@gmail.com>
2020-04-29 13:00:43 +0530

    [Test] Add tc to remove brick using nfs_ganesha
    Verify remove brick operation while IO is running
    
    Steps:
    1. Start IO on mount points
    2. Perform remove brick operation
    3. Validate IOs
    
    Change-Id: Ie394f96c9180be57704ca637c8cd725af82323cb
    Co-authored-by: Jilju Joy <jijoy@redhat.com>
    Signed-off-by: Jilju Joy <jijoy@redhat.com>
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/nfs_ganesha/test_ganesha_remove_brick.py
@@ -0,0 +1,152 @@
+#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from glusto.core import Glusto as g
+from glustolibs.gluster.nfs_ganesha_libs import NfsGaneshaClusterSetupClass
+from glustolibs.gluster.gluster_base_class import runs_on
+from glustolibs.gluster.exceptions import ExecutionError
+from glustolibs.misc.misc_libs import upload_scripts
+from glustolibs.io.utils import validate_io_procs, get_mounts_stat
+from glustolibs.gluster.volume_libs import (
+    log_volume_info_and_status, shrink_volume,
+    wait_for_volume_process_to_be_online)
+
+
+@runs_on([['distributed', 'distributed-arbiter',
+           'distributed-replicated', 'distributed-dispersed'],
+          ['nfs']])
+class TestGaneshaRemoveBrick(NfsGaneshaClusterSetupClass):
+    """
+    This test case validates remove brick functionality on volumes exported
+    through nfs-ganesha
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Setup nfs-ganesha if not exists.
+        Upload IO scripts to clients
+        """
+        cls.get_super_method(cls, 'setUpClass')()
+
+        # Setup nfs-ganesha if not exists.
+        ret = cls.setup_nfs_ganesha()
+        if not ret:
+            raise ExecutionError("Failed to setup nfs-ganesha cluster")
+        g.log.info("nfs-ganesha cluster is healthy")
+
+        # Upload IO scripts for running IO on mounts
+        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
+                                  "file_dir_ops.py")
+        ret = upload_scripts(cls.clients, cls.script_upload_path)
+        if not ret:
+            raise ExecutionError("Failed to upload IO scripts to clients %s" %
+                                 cls.clients)
+        g.log.info("Successfully uploaded IO scripts to clients %s",
+                   cls.clients)
+
+    def setUp(self):
+        """
+        Setup Volume and Mount Volume
+        """
+        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
+        if not ret:
+            raise ExecutionError("Failed to setup and mount volume %s"
+                                 % self.volname)
+        g.log.info("Successful in setup and mount volume %s", self.volname)
+
+    def test_nfs_ganesha_remove_brick(self):
+        """
+        Verify remove brick operation while IO is running
+        Steps:
+        1. Start IO on mount points
+        2. Perform remove brick operation
+        3. Validate IOs
+        """
+        # pylint: disable=too-many-statements
+        # Start IO on all mount points
+        all_mounts_procs, count = [], 1
+        for mount_obj in self.mounts:
+            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
+                   "--dirname-start-num %d "
+                   "--dir-depth 2 "
+                   "--dir-length 10 "
+                   "--max-num-of-dirs 5 "
+                   "--num-of-files 5 %s" % (self.script_upload_path, count,
+                                            mount_obj.mountpoint))
+            proc = g.run_async(mount_obj.client_system, cmd,
+                               user=mount_obj.user)
+            all_mounts_procs.append(proc)
+            count += 10
+
+        # Get stat of all the files/dirs created.
+        ret = get_mounts_stat(self.mounts)
+        self.assertTrue(ret, "Stat failed on some of the clients")
+        g.log.info("Successfully got stat of all files/dirs created")
+
+        # Perform remove brick operation
+        ret = shrink_volume(self.mnode, self.volname)
+        self.assertTrue(ret, ("Remove brick operation failed on "
+                              "%s", self.volname))
+        g.log.info("Remove brick operation is successful on "
+                   "volume %s", self.volname)
+
+        # Wait for volume processes to be online
+        ret = wait_for_volume_process_to_be_online(self.mnode, self.volname)
+        self.assertTrue(ret, ("All volume %s processes failed to come up "
+                              "online", self.volname))
+        g.log.info("All volume %s processes came up "
+                   "online successfully after remove brick operation",
+                   self.volname)
+
+        # Log volume info and status after performing remove brick
+        ret = log_volume_info_and_status(self.mnode, self.volname)
+        self.assertTrue(ret, ("Logging volume info and status failed on "
+                              "volume %s", self.volname))
+        g.log.info("Successful in logging volume info and status of volume %s",
+                   self.volname)
+
+        # Validate IO
+        ret = validate_io_procs(all_mounts_procs, self.mounts)
+        self.assertTrue(ret, "IO failed on some of the clients")
+        g.log.info("Successfully validated all io's")
+
+        # Get stat of all the files/dirs created.
+        ret = get_mounts_stat(self.mounts)
+        self.assertTrue(ret, "Stat failed on some of the clients")
+        g.log.info("Successfully got stat of all files/dirs created")
+
+    def tearDown(self):
+        """
+        Unmount and cleanup volume
+        """
+        # Unmount volume
+        ret = self.unmount_volume(self.mounts)
+        if ret:
+            g.log.info("Successfully unmounted the volume")
+        else:
+            g.log.error("Failed to unmount volume")
+
+        # Cleanup volume
+        ret = self.cleanup_volume()
+        if not ret:
+            raise ExecutionError("Failed to cleanup volume")
+        g.log.info("Cleanup volume %s completed successfully", self.volname)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_super_method(cls, 'tearDownClass')(
+            delete_nfs_ganesha_cluster=False)
