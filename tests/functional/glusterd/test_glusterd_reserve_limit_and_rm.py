6d3668924078746ba85c59e2586b289cc9549447
srijan-sivakumar <ssivakum@redhat.com>
2020-09-25 10:03:39 +0530

    [Test] Test GlusterD reserve limit and rm the files.
    Steps-
    1. Create a distribute-replicated volume and start it.
    2. Enable storage.reserve limit on the created volume.
    3. Mount the volume.
    4. Create files on the mountpoint till the storage
    reaches 100 %.
    5. Delete all the files on the mountpoint.
    
    Change-Id: Id8b03b6792bcf18dcddd730a415da63d852d5156
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

---
+++ tests/functional/glusterd/test_glusterd_reserve_limit_and_rm.py
@@ -0,0 +1,167 @@
+#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+Description:
+    Testing Reserve limit in GlusterD
+"""
+
+from glusto.core import Glusto as g
+from glustolibs.gluster.exceptions import ExecutionError
+from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
+from glustolibs.gluster.volume_libs import (
+    log_volume_info_and_status,
+    cleanup_volume,
+    setup_volume)
+from glustolibs.gluster.volume_ops import (
+    set_volume_options,
+    get_volume_list)
+from glustolibs.gluster.mount_ops import (
+    mount_volume,
+    umount_volume)
+from glustolibs.gluster.brick_libs import get_all_bricks
+from glustolibs.io.utils import wait_for_io_to_complete
+from glustolibs.misc.misc_libs import upload_scripts
+
+
+@runs_on([['distributed-replicated'], ['glusterfs']])
+class TestGlusterDReserveLimitAndRM(GlusterBaseClass):
+    """ Testing Reserve limit in GlusterD """
+
+    def setUp(self):
+        """Setup Volume"""
+        # Calling GlusterBaseClass setUp
+        self.get_super_method(self, 'setUp')()
+        self.all_mounts_procs = []
+
+        # Upload IO script for running IO on mounts
+        self.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
+                                   "file_dir_ops.py")
+        ret = upload_scripts(self.mounts[0].client_system,
+                             self.script_upload_path)
+        if not ret:
+            raise ExecutionError("Failed to upload IO scripts to client")
+
+    def test_glusterd_reserve_limit_and_rm(self):
+        """
+        Test GlusterD Reserve limit and rm the files.
+        1. Create a Distributed-replicated volume and start it.
+        2. Enable storage.reserve limit on the created volume.
+        3. Mount the volume on a client.
+        4. Create files on mountpoint till the storage limit reaches 100%
+        5. Delete all the data on mountpoint.
+        """
+        # Create and start a volume
+        ret = setup_volume(self.mnode, self.all_servers_info, self.volume)
+        self.assertTrue(ret, "Failed to create and start volume")
+
+        # Getting the list of bricks.
+        self.brick_list = get_all_bricks(self.mnode, self.volname)
+        self.assertIsNotNone(self.brick_list, "Failed to get the brick list")
+        g.log.info("Successfully got the list of bricks of volume")
+
+        # Log volume info and status before setting reserve option
+        log_volume_info_and_status(self.mnode, self.volname)
+
+        # Setting storage.reserve to 99
+        ret = set_volume_options(self.mnode, self.volname,
+                                 {'storage.reserve': '99'})
+        self.assertTrue(ret, "Failed to set storage reserve on %s"
+                        % self.mnode)
+        g.log.info("Storage reserve set successfully on %s", self.mnode)
+
+        # Log volume info and status after setting reserve options
+        log_volume_info_and_status(self.mnode, self.volname)
+
+        # Mounting the volume.
+        ret, _, _ = mount_volume(self.volname, mtype=self.mount_type,
+                                 mpoint=self.mounts[0].mountpoint,
+                                 mserver=self.mnode,
+                                 mclient=self.mounts[0].client_system)
+        self.assertEqual(ret, 0, ("Volume %s is not mounted") % self.volname)
+        g.log.info("Volume mounted successfully : %s", self.volname)
+
+        # Getting the available space on mount.
+        cmd = ("df --output=avail %s | grep '[0-9]'"
+               % self.mounts[0].mountpoint)
+        ret, out, err = g.run(self.mounts[0].client_system, cmd)
+        self.assertFalse(ret, err)
+        space_on_mount = int(out)
+
+        # Computing the number of 1 KB files that'd violate the limit set.
+        file_count = round(space_on_mount)+10
+
+        # Do some IO till the reserve limit is breached.
+        cmd = (
+            "/usr/bin/env python %s create_files "
+            "-f %d --fixed-file-size 1k --base-file-name file %s"
+            % (self.script_upload_path, file_count, self.mounts[0].mountpoint))
+        proc = g.run_async(
+            self.mounts[0].client_system, cmd, user=self.mounts[0].user)
+        self.all_mounts_procs.append(proc)
+
+        # Wait for IO to fail as reserve limit is violated.
+        self.assertFalse(wait_for_io_to_complete(self.all_mounts_procs,
+                                                 self.mounts[0]),
+                         "IO should have failed!")
+        g.log.info("IO failed due to reserve limit being breached.")
+
+        # The available space at mountpoint should be 0.
+        cmd = ("df --output=avail %s | grep '[0-9]'"
+               % self.mounts[0].mountpoint)
+        ret, out, err = g.run(self.mounts[0].client_system, cmd)
+        self.assertEqual(int(out), 0, "Memory usage is not 100%.")
+        g.log.info("Memory usage is 100%")
+
+        # Delete all files at mountpoint
+        cmd = ("cd %s/; rm -rf *" % (self.mounts[0].mountpoint))
+        ret, _, _ = g.run(self.clients[0], cmd)
+        self.assertEqual(ret, 0, "Failed to remove the files from mountpoint")
+        g.log.info("Removed all files from mountpoint")
+
+    def tearDown(self):
+        """ tear Down Callback """
+        # Unmount volume
+        ret = umount_volume(mclient=self.mounts[0].client_system,
+                            mpoint=self.mounts[0].mountpoint)
+        if not ret:
+            raise ExecutionError("Failed to Unmount the volume %s"
+                                 % self.volname)
+        g.log.info("Successful in Unmount of volume : %s", self.volname)
+
+        # Cleanup the volume
+        vol_list = get_volume_list(self.mnode)
+        if not vol_list:
+            raise ExecutionError("Failed to get the volume list")
+
+        for volume in vol_list:
+            ret = cleanup_volume(self.mnode, volume)
+            if not ret:
+                raise ExecutionError("Unable to delete volume %s"
+                                     % self.volname)
+            g.log.info("Volume deleted successfully : %s", volume)
+
+        # Cleaning up the deleted volume bricks
+        for brick in self.brick_list:
+            node, brick_path = brick.split(r':')
+            cmd = "rm -rf " + brick_path
+            ret, _, _ = g.run(node, cmd)
+            if ret:
+                raise ExecutionError("Failed to delete the brick "
+                                     "dirs of deleted volume")
+
+        # Calling GlusterBaseClass tearDown
+        self.get_super_method(self, 'tearDown')()
