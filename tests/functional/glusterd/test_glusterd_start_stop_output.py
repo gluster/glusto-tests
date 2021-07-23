9973f82798e42f0b30949271bf784ffb243465ab
Leela Venkaiah G <lgangava@redhat.com>
2021-07-23 13:16:49 +0530

    [TestFix] Set1: Review of failing glusterd tests
    Change-Id: Ia5a3f7df990360bbdc964c49496dea7399d499ab
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>

--- tests/functional/glusterd/test_glusterd_start_stop_output.py
+++ tests/functional/glusterd/test_glusterd_start_stop_output.py
@@ -13,21 +13,32 @@
 #  You should have received a copy of the GNU General Public License along
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 """Test Description:
     Consistent info display while starting and stopping glusterd.
 """
 
 from time import sleep
+
 from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
+
+from glustolibs.gluster.gluster_base_class import GlusterBaseClass
 from glustolibs.gluster.gluster_init import wait_for_glusterd_to_start
 
 
-@runs_on([['replicated', 'distributed', 'arbiter', 'dispersed',
-           'distributed-replicated', 'distributed-arbiter',
-           'distributed-dispersed'], ['glusterfs']])
 class TestGlusterdStartStopOutput(GlusterBaseClass):
+    # It might so happen that glusterd is restarted in previous tests and
+    # so reset the count before and after the test
+    def setUp(self):
+        self.get_super_method(self, 'setUp')()
+        self.glusterd_start_cmd = 'systemctl start glusterd'
+        self.glusterd_stop_cmd = 'systemctl stop glusterd'
+        self._run_command_in_servers('systemctl reset-failed glusterd')
+
+    def tearDown(self):
+        self._run_command_in_servers('systemctl reset-failed glusterd')
+        self._run_command_in_servers(self.glusterd_start_cmd)
+        self.get_super_method(self, 'tearDown')()
+
     def _run_command_in_servers(self, cmd):
         """
         Function to run a given command in the servers and validate if they
@@ -44,26 +55,25 @@ class TestGlusterdStartStopOutput(GlusterBaseClass):
         2. Check if consistent output is returned if glusterd is started and
         stopped 5 times.
         """
-        glusterd_start_cmd = 'systemctl start glusterd'
-        glusterd_stop_cmd = 'systemctl stop glusterd'
-
         # Stop glusterd to get the info for further comparison.
-        stop_output = self._run_command_in_servers(glusterd_stop_cmd)
+        stop_output = self._run_command_in_servers(self.glusterd_stop_cmd)
 
         # Start glusterd to get the info for further comparison.
-        start_output = self._run_command_in_servers(glusterd_start_cmd)
+        start_output = self._run_command_in_servers(self.glusterd_start_cmd)
 
         # Loop over the stop-stop cycle for 5 times and validate the output.
         for _ in range(5):
             # Validate stop output is the same.
-            ret_value = self._run_command_in_servers(glusterd_stop_cmd)
-            self.assertEqual(ret_value, stop_output,
-                             "%s is not consistent." % (glusterd_stop_cmd))
+            ret_value = self._run_command_in_servers(self.glusterd_stop_cmd)
+            self.assertEqual(
+                ret_value, stop_output,
+                "%s is not consistent." % (self.glusterd_stop_cmd))
             sleep(2)
 
             # Validate start output is the same.
-            ret_value = self._run_command_in_servers(glusterd_start_cmd)
-            self.assertEqual(ret_value, start_output,
-                             "%s is not consistent." % (glusterd_start_cmd))
+            ret_value = self._run_command_in_servers(self.glusterd_start_cmd)
+            self.assertEqual(
+                ret_value, start_output,
+                "%s is not consistent." % (self.glusterd_start_cmd))
             ret = wait_for_glusterd_to_start(self.servers[0])
             self.assertTrue(ret, "Glusterd has not yet started in servers.")
